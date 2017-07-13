3891c700c4ae826b289a09d588680201ce6a987d
Karthik Ram <karthik.ram@gmail.com>
2017-07-13 15:53:51 -0700

    Added a draft of the new theme

---
+++ themes/ropensci/static/js/tutorialinit.js
@@ -0,0 +1,18 @@
+$(document).ready( function () {
+
+    $('#tutorialtable').DataTable({
+        "info": false, // won't display showed entries of total
+        "pagingType": "simple_numbers",
+        "pageLength": 18,
+        "lengthChange": false, // Disables ability to change results number per page
+        "language": {
+            "search": ' ', // Changes 'Search' label value
+            "searchPlaceholder": "Type to search…", // adds placeholder text to search field
+            "paginate": {
+                "previous": "Prev", //changes 'Previous' label value
+            } 
+        }
+    });
+
+} );
+
