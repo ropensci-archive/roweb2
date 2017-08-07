1825e7a2b08aeb304912d678f0ad2ab8bd0d0b39
marulango <hola@maruma.ru>
2017-08-07 14:56:00 -0700

    Big change to calinit

--- themes/ropensci/static/js/calinit.js
+++ themes/ropensci/static/js/calinit.js
@@ -1,151 +1,402 @@
-//replace this with proper public calendar client_id
-var CLIENT_ID = '237243040571-3gjdcv4ul9kdivml6pu725qcs2aclpfk.apps.googleusercontent.com'; 
+/**
+ * Format Google Calendar JSON output into human readable list
+ *
+ * Copyright 2015, Milan Kacurak
+ *
+ */
+var formatGoogleCalendar = (function () {
+  'use strict'
 
-var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+  var config
 
-function checkAuth() {
-    gapi.auth.authorize(
-    {
-        'client_id': CLIENT_ID,
-        'scope': SCOPES.join(' '),
-        'immediate': true
-    }, handleAuthResult);
-}
+    // Gets JSON from Google Calendar and transfroms it into html list items and appends it to past or upcoming events list
+  var init = function (settings) {
+    var result = []
 
+    config = settings
+    var finalURL = settings.calendarUrl
 
-function handleAuthResult(authResult) {
-    var authorizeDiv = document.getElementById('authorize-div');
-    if (authResult && !authResult.error) {
-        // Hide auth UI, then load client library.
-        authorizeDiv.style.display = 'none';
-        loadCalendarApi();
-    } else {
-        // Show auth UI, allowing the user to initiate authorization by
-        // clicking authorize button.
-        authorizeDiv.style.display = 'inline';
+    if (settings.recurringEvents) finalURL = finalURL.concat('&singleEvents=true')
+
+        // Get JSON, parse it, transform into list items and append it to past or upcoming events list
+    jQuery.getJSON(finalURL, function (data) {
+            // Remove any cancelled events
+      data.items.forEach(function removeCancelledEvents (item) {
+        if (item && item.hasOwnProperty('status') && item.status !== 'cancelled') {
+          result.push(item)
         }
-    }
+      })
 
-/**
-* Initiate auth flow in response to user clicking authorize button.
-*
-* @param {Event} event Button click event.
-*/
-function handleAuthClick(event) {
-    gapi.auth.authorize({
-        client_id: CLIENT_ID, 
-        scope: SCOPES, 
-        immediate: false
-    },
-    handleAuthResult );
-    return false;
-}
+      result.sort(comp).reverse()
 
-/**
-* Load Google Calendar client library. List upcoming events
-* once client library is loaded.
-*/
-function loadCalendarApi() {
-    gapi.client.load('calendar', 'v3', listUpcomingEvents);
-}
+      var pastCounter = 0,
+        upcomingCounter = 0,
+        pastResult = [],
+        upcomingResult = [],
+        upcomingResultTemp = [],
+        attendees = [],
+        $upcomingElem = jQuery(settings.upcomingSelector),
+        $pastElem = jQuery(settings.pastSelector),
+        i
 
-/**
-* Print the summary and start datetime/date of the next ten events in
-* the authorized user's calendar. If no events are found an
-* appropriate message is printed.
-*/
-function listUpcomingEvents() {
-    var request = gapi.client.calendar.events.list({
-        'calendarId': 'primary',
-        'timeMin': (new Date()).toISOString(),
-        'singleEvents': true,
-        'maxResults': 10,
-        'orderBy': 'startTime',
-    });
+      if (settings.pastTopN === -1) {
+        settings.pastTopN = result.length
+      }
 
-    request.execute(processItems);
-}
+      if (settings.upcomingTopN === -1) {
+        settings.upcomingTopN = result.length
+      }
 
-function processItems (resp) {
-    var events = resp.items;
-    var puchi = document.getElementById('eventlist');
-    if (events.length === 0) {
-        puchi.innerHTML = '<h4>No upcoming events found</h4>';
-        return;
-    }
+      if (settings.past === false) {
+        settings.pastTopN = 0
+      }
 
-    var frag = document.createDocumentFragment();
+      if (settings.upcoming === false) {
+        settings.upcomingTopN = 0
+      }
 
-    for (i = 0; i < events.length; i++) {
-        var event = events[i];
-
-        var msg = createTr(event)
-        frag.appendChild(msg)
-    }
-    puchi.appendChild(frag);
-
-}
-
-
-function createTr(event) {
-
-    var start = event.start.dateTime;
-    if (!start) {
-        start = event.start.date;
-    }
-
-    if ( event.summary == null ) {
-        var summary =  'N/A'
-    } else {
-        var summary = event.summary
-    }
-
-    //TODO: Invite self to calendar event to create attendees array and print that :D
-    var linkNode = document.createElement('a')
-    var htmlLink = event.htmlLink 
-    linkNode.href = htmlLink
-    var summaryContent = document.createTextNode(summary)
-    linkNode.appendChild(summaryContent)
-
-    if (event.description == null  ) {
-        var details = 'TBA'
-    } else {
-        var details = event.description
-    }
-
-    var attendees = event.attendees
-    var name = '';
-    if ( attendees == null ) {
-        var name = 'TBA'
-    }
-    for ( i = 1;  i < attendees.length; i++ ){
-        if ( i < attendees.length - 1 ) {
-            name = name + attendees[i].displayName + ', '
-        } else if( i < attendees.length ) {
-            name = name + attendees[i].displayName 
+      for (i in result) {
+        if (isPast(result[i].end.dateTime || result[i].end.date)) {
+          if (pastCounter < settings.pastTopN) {
+            pastResult.push(result[i])
+            pastCounter++
+          }
+        } else {
+          upcomingResultTemp.push(result[i])
         }
+      }
+
+      upcomingResultTemp.reverse()
+
+      for (i in upcomingResultTemp) {
+        if (upcomingCounter < settings.upcomingTopN) {
+          upcomingResult.push(upcomingResultTemp[i])
+          upcomingCounter++
+        }
+      }
+
+      for (i in pastResult) {
+        $pastElem.append(transformationList(pastResult[i], settings.itemsTagName, settings.format))
+      }
+
+      for (i in upcomingResult) {
+        $upcomingElem.append(transformationList(upcomingResult[i], settings.itemsTagName, settings.format))
+        var attendees = upcomingResult[i].attendees
+        for (i in attendees) {
+          if (i < attendees.length - 1) {
+            name = attendees[i].displayName + ', '
+          } else if (i < attendees.length) {
+            name = attendees[i].displayName
+          }
+        }
+      }
+    })
+  }
+
+    // Compare dates
+  var comp = function (a, b) {
+    return new Date(a.start.dateTime || a.start.date).getTime() - new Date(b.start.dateTime || b.start.date).getTime()
+  }
+
+    // Overwrites defaultSettings values with overrideSettings and adds overrideSettings if non existent in defaultSettings
+  var mergeOptions = function (defaultSettings, overrideSettings) {
+    var newObject = {},
+      i
+    for (i in defaultSettings) {
+      newObject[i] = defaultSettings[i]
+    }
+    for (i in overrideSettings) {
+      newObject[i] = overrideSettings[i]
+    }
+    return newObject
+  }
+
+  var isAllDay = function (dateStart, dateEnd) {
+    var dateStartFormatted = getDateFormatted(dateStart),
+      dateEndFormatted = getDateFormatted(dateEnd)
+
+      // if start date is midnight and the end date a following day midnight as well
+    if ((dateStartFormatted.getTime() === dateEndFormatted.getTime() - 86400000) &&
+          dateStartFormatted.getMinutes() === 0 &&
+          dateStartFormatted.getHours() === 0) {
+      return true
     }
 
-    if (event.location == null  ) {
-        var location = 'TBA'
+    return false
+  }
+
+    // Get all necessary data (dates, location, summary, description) and creates a list item
+  var transformationList = function (result, tagName, format) {
+    var dateStart = getDateInfo(result.start.dateTime || result.start.date),
+      dateEnd = getDateInfo(result.end.dateTime || result.end.date),
+      moreDaysEvent = (typeof result.end.date !== 'undefined'),
+      dayNames = config.dayNames,
+      isAllDayEvent = isAllDay(dateStart, dateEnd)
+
+    if (moreDaysEvent) {
+      dateStart = addOneDay(dateStart)
+    }
+
+    if (isAllDayEvent) {
+      dateEnd = subtractOneMinute(dateEnd)
+    }
+
+    var dateFormatted = getFormattedDate(dateStart, dateEnd, moreDaysEvent, isAllDayEvent, dayNames),
+      output = '<' + tagName + '>',
+      summary = result.summary || '',
+      description = result.description || '',
+      location = result.location || '',
+      i
+
+    for (i = 0; i < format.length; i++) {
+      format[i] = format[i].toString()
+
+      if (format[i] === '*summary*') {
+        output = output.concat('<td class="summary">' + summary + '</td>')
+      } else if (format[i] === '*date*') {
+        output = output.concat('<td class="date">' + dateFormatted + '</td>')
+      } else if (format[i] === '*description*') {
+        output = output.concat('<td class="description">' + description + '</td>')
+      } else if (format[i] === '*location*') {
+        output = output.concat('<td class="location">' + location + '</td>')
+      } else if (format[i] === '*attendees*') {
+        output = output.concat('<td class="attendees">' + name + '</td>')
+      } else {
+        if ((format[i + 1] === '*location*' && location !== '') ||
+                    (format[i + 1] === '*summary*' && summary !== '') ||
+                    (format[i + 1] === '*date*' && dateFormatted !== '') ||
+                    (format[i + 1] === '*description*' && description !== '') ||
+                    (format[i + 1] === '*attendees*' && name !== '')) {
+          output = output.concat(format[i])
+        }
+      }
+    }
+
+    return output + '</' + tagName + '>'
+  }
+
+    // Check if date is later then now
+  var isPast = function (date) {
+    var compareDate = new Date(date),
+      now = new Date()
+
+    if (now.getTime() > compareDate.getTime()) {
+      return true
+    }
+
+    return false
+  }
+
+    // Get temp array with information abou day in followin format: [day number, month number, year, hours, minutes]
+  var getDateInfo = function (date) {
+    date = new Date(date)
+    return [date.getDate(), date.getMonth(), date.getFullYear(), date.getHours(), date.getMinutes(), 0, 0]
+  }
+
+    // Get month name according to index
+  var getMonthName = function (month) {
+    var monthNames = [
+      'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+    ]
+
+    return monthNames[month]
+  }
+
+  var getDayName = function (day) {
+    var dayNames = [
+      'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
+    ]
+
+    return dayNames[day]
+  }
+
+  var getDayNameFormatted = function (dateFormatted) {
+    return getDayName(getDateFormatted(dateFormatted).getDay()) + ' '
+  }
+
+  var getDateFormatted = function (dateInfo) {
+    return new Date(dateInfo[2], dateInfo[1], dateInfo[0], dateInfo[3], dateInfo[4] + 0, 0)
+  }
+
+    // Add one day
+  var addOneDay = function (dateInfo) {
+    var date = getDateFormatted(dateInfo)
+    date.setTime(date.getTime() + 86400000)
+    return getDateInfo(date)
+  }
+
+    // Subtract one day
+  var subtractOneDay = function (dateInfo) {
+    var date = getDateFormatted(dateInfo)
+    date.setTime(date.getTime() - 86400000)
+    return getDateInfo(date)
+  }
+
+    // Subtract one minute
+  var subtractOneMinute = function (dateInfo) {
+    var date = getDateFormatted(dateInfo)
+    date.setTime(date.getTime() - 60000)
+    return getDateInfo(date)
+  }
+
+    // Transformations for formatting date into human readable format
+  var formatDateSameDay = function (dateStart, dateEnd, moreDaysEvent, isAllDayEvent, dayNames) {
+    var formattedTime = '',
+      dayNameStart = ''
+
+    if (dayNames) {
+      dayNameStart = getDayNameFormatted(dateStart)
+    }
+
+    if (config.sameDayTimes && !moreDaysEvent && !isAllDayEvent) {
+      formattedTime = ' from ' + getFormattedTime(dateStart) + ' - ' + getFormattedTime(dateEnd)
+    }
+
+        // month day, year time-time
+    return dayNameStart + getMonthName(dateStart[1]) + ' ' + dateStart[0] + ', ' + dateStart[2] + formattedTime
+  }
+
+  var formatDateOneDay = function (dateStart, dayNames) {
+    var dayName = ''
+
+    if (dayNames) {
+      dayName = getDayNameFormatted(dateStart)
+    }
+      // month day, year
+    return dayName + getMonthName(dateStart[1]) + ' ' + dateStart[0] + ', ' + dateStart[2]
+  }
+
+  var formatDateDifferentDay = function (dateStart, dateEnd, dayNames) {
+    var dayNameStart = '',
+      dayNameEnd = ''
+
+    if (dayNames) {
+      dayNameStart = getDayNameFormatted(dateStart)
+      dayNameEnd = getDayNameFormatted(dateEnd)
+    }
+        // month day-day, year
+    return dayNameStart + getMonthName(dateStart[1]) + ' ' + dateStart[0] + '-' + dayNameEnd + dateEnd[0] + ', ' + dateStart[2]
+  }
+
+  var formatDateDifferentMonth = function (dateStart, dateEnd, dayNames) {
+    var dayNameStart = '',
+      dayNameEnd = ''
+
+    if (dayNames) {
+      dayNameStart = getDayNameFormatted(dateStart)
+      dayNameEnd = getDayNameFormatted(dateEnd)
+    }
+        // month day - month day, year
+    return dayNameStart + getMonthName(dateStart[1]) + ' ' + dateStart[0] + '-' + dayNameEnd + getMonthName(dateEnd[1]) + ' ' + dateEnd[0] + ', ' + dateStart[2]
+  }
+
+  var formatDateDifferentYear = function (dateStart, dateEnd, dayNames) {
+    var dayNameStart = '',
+      dayNameEnd = ''
+
+    if (dayNames) {
+      dayNameStart = getDayNameFormatted(dateStart)
+      dayNameEnd = getDayNameFormatted(dateEnd)
+    }
+        // month day, year - month day, year
+    return dayNameStart + getMonthName(dateStart[1]) + ' ' + dateStart[0] + ', ' + dateStart[2] + '-' + dayNameEnd + getMonthName(dateEnd[1]) + ' ' + dateEnd[0] + ', ' + dateEnd[2]
+  }
+
+    // Check differences between dates and format them
+  var getFormattedDate = function (dateStart, dateEnd, moreDaysEvent, isAllDayEvent, dayNames) {
+    var formattedDate = ''
+
+    if (dateStart[0] === dateEnd[0]) {
+      if (dateStart[1] === dateEnd[1]) {
+        if (dateStart[2] === dateEnd[2]) {
+                    // month day, year
+          formattedDate = formatDateSameDay(dateStart, dateEnd, moreDaysEvent, isAllDayEvent, dayNames)
+        } else {
+                    // month day, year - month day, year
+          formattedDate = formatDateDifferentYear(dateStart, dateEnd, dayNames)
+        }
+      } else {
+        if (dateStart[2] === dateEnd[2]) {
+                    // month day - month day, year
+          formattedDate = formatDateDifferentMonth(dateStart, dateEnd, dayNames)
+        } else {
+                    // month day, year - month day, year
+          formattedDate = formatDateDifferentYear(dateStart, dateEnd, dayNames)
+        }
+      }
     } else {
-        var location = event.location
+      if (dateStart[1] === dateEnd[1]) {
+        if (dateStart[2] === dateEnd[2]) {
+                    // month day-day, year
+          formattedDate = formatDateDifferentDay(dateStart, dateEnd, dayNames)
+        } else {
+                    // month day, year - month day, year
+          formattedDate = formatDateDifferentYear(dateStart, dateEnd, dayNames)
+        }
+      } else {
+        if (dateStart[2] === dateEnd[2]) {
+                    // month day - month day, year
+          formattedDate = formatDateDifferentMonth(dateStart, dateEnd, dayNames)
+        } else {
+                    // month day, year - month day, year
+          formattedDate = formatDateDifferentYear(dateStart, dateEnd, dayNames)
+        }
+      }
     }
 
-    var trNode = document.createElement('tr');
+    return formattedDate
+  }
 
-    var td = document.createElement('td');
-    td.appendChild(linkNode)
-    trNode.appendChild(td);
+  var getFormattedTime = function (date) {
+    var formattedTime = '',
+      period = 'AM',
+      hour = date[3],
+      minute = date[4]
 
-    var tdNodes = [start, location, name, details];
+        // Handle afternoon.
+    if (hour >= 12) {
+      period = 'PM'
 
-    for (var i in tdNodes) {
-        var content = tdNodes[i];
-        var tdNode = document.createElement('td');
-        tdNode.innerHTML = content;
-        trNode.appendChild(tdNode);
+      if (hour >= 13) {
+        hour -= 12
+      }
     }
 
-    return trNode;
-}
+        // Handle midnight.
+    if (hour === 0) {
+      hour = 12
+    }
+
+        // Ensure 2-digit minute value.
+    minute = (minute < 10 ? '0' : '') + minute
+
+        // Format time.
+    formattedTime = hour + ':' + minute + period
+    return formattedTime
+  }
+
+  return {
+    init: function (settingsOverride) {
+      var settings = {
+        calendarUrl: 'https://www.googleapis.com/calendar/v3/calendars/milan.kacurak@gmail.com/events?key=AIzaSyCR3-ptjHE-_douJsn8o20oRwkxt-zHStY',
+        past: true,
+        upcoming: true,
+        sameDayTimes: true,
+        dayNames: true,
+        pastTopN: -1,
+        upcomingTopN: -1,
+        recurringEvents: true,
+        itemsTagName: 'li',
+        upcomingSelector: '#events-upcoming',
+        pastSelector: '#events-past',
+        upcomingHeading: '<h2>Upcoming events</h2>',
+        pastHeading: '<h2>Past events</h2>',
+        format: ['*date*', ': ', '*summary*', ' &mdash; ', '*description*', ' in ', '*location*']
+      }
+
+      settings = mergeOptions(settings, settingsOverride)
+
+      init(settings)
+    }
+  }
+})()
