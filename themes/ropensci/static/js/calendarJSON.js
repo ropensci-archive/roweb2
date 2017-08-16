90629237ebceca97b45f538602787556fec65497
marulango <hola@maruma.ru>
2017-08-15 21:37:27 -0700

    adds calendar json sample file

---
+++ themes/ropensci/static/js/calendarJSON.js
@@ -0,0 +1,28 @@
+var calendar = {
+  'title': 'rOpenSci Public Calendar',
+  'link': 'https://calendar.google.com/calendar/embed?src=t6pva0jm24urp7cp0n32262jd8%40group.calendar.google.com&ctz=America/Los_Angeles',
+  'description': 'the rOpenSci public events calendar',
+  'eventList': [
+    {
+      'title': 'An article',
+      'dateStart': 'August 20 2017, 8:00 ',
+      'dateEnd': 'August 21 2017, 19:00',
+      'description': 'Some sample text here',
+      'attendees': 'example author'
+    },
+    {
+      'title': 'Some workshop',
+      'dateStart': 'August 20 2017, 8:00 ',
+      'dateEnd': 'August 26 2017, 19:00',
+      'description': 'Some sample text here',
+      'attendees': 'example author'
+    },
+    {
+      'title': 'Community Call',
+      'dateStart': 'August 20 2017, 8:00 ',
+      'dateEnd': 'August 20 2017, 19:00',
+      'description': 'Some sample text here',
+      'attendees': 'example author'
+    }
+  ]
+}
