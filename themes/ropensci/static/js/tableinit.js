a33333f5216c0e70b21781bdf9330d9450f15117
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-10-18 08:52:37 +0200

    beautify

--- themes/ropensci/static/js/tableinit.js
+++ themes/ropensci/static/js/tableinit.js
@@ -1,66 +1,72 @@
 var oTable;
-$(document).ready( function () {
+$(document).ready(function() {
     var markdown = new showdown.Converter();
 
     oTable = $('#packagestable').DataTable({
-        "ajax" : {
-            "url" : "https://gist.github.com/maelle/9d7542859d1c998d0b4c4ad3905cfc70/raw/18e1d46bb3b5e365786994a83c76606f3683cc6e/registry.json",
+        "ajax": {
+            "url": "https://gist.github.com/maelle/9d7542859d1c998d0b4c4ad3905cfc70/raw/18e1d46bb3b5e365786994a83c76606f3683cc6e/registry.json",
             "dataSrc": "packages"
         },
-        "columns": [
-            { "data" : function(row, type, set, meta){
-                return '<a href="' + row.url + '">' + row.name + '</a>';
+        "columns": [{
+                "data": function(row, type, set, meta) {
+                    return '<a href="' + row.url + '">' + row.name + '</a>';
+                },
+                "title": "name"
             },
-              "title": "name"
+            {
+                data: 'status',
+                render: {
+                    display: 'badge',
+                    sort: 'status',
+                    filter: 'status',
+                    type: 'status'
+                },
+                title: "status"
             },
-            { data: 'status',
-    render: {
-        display: 'badge',
-        sort: 'status',
-        filter: 'status',
-        type: 'status'
-    },
-              title: "status"
+            {
+                "data": 'maintainer',
+                "title": "maintainer"
             },
-            { "data": 'maintainer',
-            "title": "maintainer"
+            {
+                "data": function(row, type, set, meta) {
+                    return markdown.makeHtml(row.description);
+                },
+                "title": "description",
+                "searchable": "true"
             },
-            { "data": function(row, type, set, meta){
-                return markdown.makeHtml(row.description);
+            {
+                data: 'on_cran',
+                render: {
+                    display: 'badge',
+                    sort: 'on_cran',
+                    filter: 'on_cran',
+                    type: 'on_cran'
+                },
+                title: "CRAN/Bioc"
             },
-              "title": "description",
-              "searchable": "true"
+            {
+                data: 'onboarding',
+                render: {
+                    display: 'badge',
+                    sort: 'review',
+                    filter: 'review',
+                    type: 'review'
+                },
+                title: "onboarding"
             },
-            { data: 'on_cran',
-    render: {
-        display: 'badge',
-        sort: 'on_cran',
-        filter: 'on_cran',
-        type: 'on_cran'
-    },
-              title: "CRAN/Bioc"
-            },
-            { data: 'onboarding',
-    render: {
-        display: 'badge',
-        sort: 'review',
-        filter: 'review',
-        type: 'review'
-    },
-              title: "onboarding"
-            },
-            { "data": function(row, type, set, meta){
-                return row.keywords;
-            },
-              "visible": "false"
+            {
+                "data": function(row, type, set, meta) {
+                    return row.keywords;
+                },
+                "visible": "false"
             }
         ],
-        "createdRow" : function( row, data, index ){
+        "createdRow": function(row, data, index) {
             // combine some small categories
-            if(data.ropensci_category == "data-analysis" || data.ropensci_category == "metadata")
-               data.ropensci_category =  "data-tools";
+            if (data.ropensci_category == "data-analysis" || data.ropensci_category == "metadata")
+                data.ropensci_category = "data-tools";
             $(row).addClass(data.ropensci_category);
-            if(data.on_cran)
+            if (data.on_cran)
                 $(row).addClass('on_cran');
         },
         "info": false, // won't display showed entries of total
