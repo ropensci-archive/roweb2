7b7dd269ac8d427fa75bbf91bf3d30552353093b
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-10-18 09:02:00 +0200

    nothing

--- themes/ropensci/static/js/tableinit.js
+++ themes/ropensci/static/js/tableinit.js
@@ -69,26 +69,6 @@ $(document).ready(function() {
             "paginate": {
                 "previous": "Prev", //changes 'Previous' label value
             }
-        },
-        initComplete: function () {
-            this.api().columns().every( function () {
-                var column = this;
-                var select = $('<select><option value=""></option></select>')
-                    .appendTo( $(column.footer()).empty() )
-                    .on( 'change', function () {
-                        var val = $.fn.dataTable.util.escapeRegex(
-                            $(this).val()
-                        );
- 
-                        column
-                            .search( val ? '^'+val+'$' : '', true, false )
-                            .draw();
-                    } );
- 
-                column.data().unique().sort().each( function ( d, j ) {
-                    select.append( '<option value="'+d+'">'+d+'</option>' )
-                } );
-            } );
         }
     });
 
