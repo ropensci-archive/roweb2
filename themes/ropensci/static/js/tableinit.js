145a306fd49087273a71ee75de6d250c04353a7b
Maëlle Salmon <maelle.salmon@yahoo.se>
2018-10-25 08:58:08 +0200

    a bit more change

--- themes/ropensci/static/js/tableinit.js
+++ themes/ropensci/static/js/tableinit.js
@@ -5,31 +5,65 @@ $(document).ready( function () {
     var markdown = new showdown.Converter();
 
     oTable = $('#packagestable').DataTable({
-        "ajax" : {
-            "url" : "https://ropensci.github.io/roregistry/registry.json",
+        "ajax": {
+            "url": "https://gist.githubusercontent.com/maelle/9d7542859d1c998d0b4c4ad3905cfc70/raw/18e1d46bb3b5e365786994a83c76606f3683cc6e/registry.json",
             "dataSrc": "packages"
         },
         "columns": [
-            { "data" : function(row, type, set, meta){
-                return '<a href="' + row.url + '">' + row.name + '</a>';
-            }},
-            { "data": function(row, type, set, meta){
+          {
+                "data": function(row, type, set, meta) {
+                    return '<a href="' + row.url + '">' + row.name + '</a>';
+                },
+                "title": "name"
+            },
+            {
+                data: 'status',
+                render: {
+                    display: 'badge',
+                    sort: 'status',
+                    filter: 'status',
+                    type: 'status',
+                    _: 'status'
+                    },
+                title: "status"
+            },
+            {
+                "data": function(row, type, set, meta){
                 return '<a href="#packagestable" onclick="oTable.search(\'' + row.maintainer + '\').draw();">' + row.maintainer + '</a>';
-            }},
-            { "data": function(row, type, set, meta){
-                return markdown.makeHtml(row.description);
-            }},
-            { "data": function(row, type, set, meta){
-                var src = '<a target="_blank" href="' + row.url + '"><p class="label icon-github"></p></a>';
-                if(row.on_cran){
-                    src = '<a target="_blank" href="https://cran.r-project.org/package=' + row.name + '"><p class="label cran">cran</p></a>' + src;
-                } else if(row.on_bioc){
-                    src = '<a target="_blank" href="https://bioconductor.org/packages/release/bioc/html/' + row.name + '.html"><p class="label bioc">bioc</p></a>' + src;
-                } else {
-                    src = '<p class="label nocran">cran</p>' + src;
-                }
-                return src;
-            }}
+},
+                "title": "maintainer",
+                "name": "select"
+            },
+            {
+                "data": function(row, type, set, meta) {
+                    return markdown.makeHtml(row.description);
+                },
+                "title": "description",
+            },
+            {
+                data: 'on_cran',
+                render: {
+                    display: 'badge',
+                    sort: 'on_cran',
+                    filter: 'on_cran',
+                    type: 'on_cran'
+                },
+                title: "CRAN/Bioc"
+            },
+            {
+                data: 'onboarding',
+                render: {
+                    display: 'badge',
+                    sort: 'review',
+                    filter: 'review',
+                    type: 'review'
+                },
+                title: "onboarding"
+            },
+            {
+                "data": 'keywords',
+                "visible": false
+            }
         ],
         "createdRow" : function( row, data, index ){
             // combine some small categories
@@ -38,18 +72,16 @@ $(document).ready( function () {
             $(row).addClass(data.ropensci_category);
             if(data.on_cran)
                 $(row).addClass('on_cran');
+            if(data.status == "active")
+                $(row).addClass('active');
         },
         "info": false, // won't display showed entries of total
         "pagingType": "simple_numbers",
         "pageLength": 18,
         "lengthChange": false, // Disables ability to change results number per page
-        "columnDefs": [{ 
-            "searchable": false, 
-            "targets": 3 
-        }],
-        "language": {
+                "language": {
             "search": ' ', // Changes 'Search' label value
-            "searchPlaceholder": "Type to search…", // adds placeholder text to search field
+            "searchPlaceholder": "Type to search on all fields incl. hidden fields like keywords", // adds placeholder text to search field
             "paginate": {
                 "previous": "Prev", //changes 'Previous' label value
             }
@@ -71,9 +103,13 @@ $(document).ready( function () {
     $.fn.dataTableExt.afnFiltering.push(
         function (oSettings, aData, iDataIndex) {
             var cran = $('input[type=checkbox]')
+            var active = $('input[type=checkbox]')
             var selected = $('input:checked')
             var filter = selected.attr('class')
             if (cran.is(':checked') && ! $(oSettings.aoData[iDataIndex].nTr).hasClass('on_cran')){
+                return false;
+            }
+            if (active.is(':checked') && ! $(oSettings.aoData[iDataIndex].nTr).hasClass('active')){
                 return false;
             }
             return !filter || filter == 'all' || $(oSettings.aoData[iDataIndex].nTr).hasClass(filter);
