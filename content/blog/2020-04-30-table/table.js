2bff6e522228c93b689124d1eccde53f76ddb451
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-05-01 15:10:08 +0200

    progress

---
+++ content/blog/2020-04-30-table/table.js
@@ -0,0 +1,73 @@
+function format ( d ) {
+    var markdown = new showdown.Converter();
+    return markdown.makeHtml(d.details)+'<br>'+
+        d.citations;
+}
+ 
+$(document).ready(function() {
+    var dt = $('#packagestable').DataTable( {
+        
+        "ajax": {
+            "url": "registry.json",
+            "dataSrc": "packages"
+        },
+        "columns": [
+            {
+                "class":          "details-control",
+                "orderable":      false,
+                "data":           null,
+                "defaultContent": ""
+            },
+            {
+                data: 'name',
+                title: "Package"
+            },
+            {
+                data: "description",
+                title: "Description",
+            },
+            {
+                data: 'data_source',
+                title: "Data Source"
+            },
+            {
+                data: 'maintainer',
+                title: "Maintainer"
+            }
+        ],
+        "order": [[1, 'asc']]
+    } );
+ 
+    // Array to track the ids of the details displayed rows
+    var detailRows = [];
+ 
+    $('#packagestable tbody').on( 'click', 'tr td.details-control', function () {
+        var tr = $(this).closest('tr');
+        var row = dt.row( tr );
+        var idx = $.inArray( tr.attr('id'), detailRows );
+ 
+        if ( row.child.isShown() ) {
+            tr.removeClass( 'details' );
+            row.child.hide();
+ 
+            // Remove from the 'open' array
+            detailRows.splice( idx, 1 );
+        }
+        else {
+            tr.addClass( 'details' );
+            row.child( format( row.data() ) ).show();
+ 
+            // Add to the 'open' array
+            if ( idx === -1 ) {
+                detailRows.push( tr.attr('id') );
+            }
+        }
+    } );
+ 
+    // On each draw, loop over the `detailRows` array and show any child rows
+    dt.on( 'draw', function () {
+        $.each( detailRows, function ( i, id ) {
+            $('#'+id+' td.details-control').trigger( 'click' );
+        } );
+    } );
+} );
