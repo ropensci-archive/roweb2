9b2702f6552770d0b18201e2694e51d07573596d
Jeroen Ooms <jeroenooms@gmail.com>
2020-05-12 19:53:24 +0200

    Package details (#694)
    * move repostatus to the right
    
    * Adding details row for packages
    
    * adding description and citation
    
    * npm run css

--- themes/ropensci/static/js/tableinit.js
+++ themes/ropensci/static/js/tableinit.js
@@ -10,17 +10,16 @@ $(document).ready( function () {
             "dataSrc": "packages"
         },
         "columns": [
-          {
-                "data" : function(row, type, set, meta){
-                return '<a target="_blank" href=\"https://www.repostatus.org/#' + row.status + '"><i class="label icon-ropensci-short ' + row.status  +'" ' + row.status + '" title = "' + row.status + ' package"></i></a>';
-},
-                title: ""
+            {
+                "className":      'details-control',
+                "orderable":      false,
+                "data":           null,
+                "defaultContent": '<i class="label fa fa-caret-right"></i>'
             },
-          
-          {
+            {
                 "data" : function(row, type, set, meta){
-                return '<a href="https://docs.ropensci.org/' + row.name + '">' + row.name + '</a>';
-},
+                    return '<a href="https://docs.ropensci.org/' + row.name + '">' + row.name + '</a>';
+                },
                 title: "Name"
             },
             {
@@ -30,8 +29,8 @@ $(document).ready( function () {
             },
             {
                 "data": function(row, type, set, meta){
-                return '<a href="#packagestable" onclick="oTable.search(\'' + row.maintainer + '\').draw();">' + row.maintainer + '</a>';
-},
+                    return '<a href="#packagestable" onclick="oTable.search(\'' + row.maintainer + '\').draw();">' + row.maintainer + '</a>';
+            },
                 "title": "maintainer"
             },
             {
@@ -41,23 +40,29 @@ $(document).ready( function () {
                 "title": "description",
             },
             {
+                "data" : function(row, type, set, meta){
+                    return '<a target="_blank" href=\"https://www.repostatus.org/#' + row.status + '"><i class="label icon-ropensci-short ' + row.status  +'" ' + row.status + '" title = "' + row.status + ' package"></i></a>';
+                },
+                title: ""
+            },
+            {
                 "data": function(row, type, set, meta){
-                var src = '';
-                if(row.on_cran){
-                    src = '<a target="_blank" href="https://cran.r-project.org/package=' + row.name + '"><p class="label cran">cran</p></a>' + src;
-                } else if(row.on_bioc){
-                    src = '<a target="_blank" href="https://bioconductor.org/packages/release/bioc/html/' + row.name + '.html"><p class="label bioc">bioc</p></a>' + src;
-                } else {
-                    src = '<p class="label nocran">cran</p>' + src;
-                }
-                if (row.onboarding){
-                  src = src + '<a target=\"_blank\" href="' + row.onboarding + '"><i class="fas fa-comments fa-onboarding yes" title = "rOpenSci software review"></i></a>'
-                } else {
-                  src = src + '<i class="fas fa-comments fa-onboarding no"></i>'
-                }
-                return src;
-},
-                title: "Details"
+                    var src = '';
+                    if(row.on_cran){
+                        src = '<a target="_blank" href="https://cran.r-project.org/package=' + row.name + '"><p class="label cran">cran</p></a>' + src;
+                    } else if(row.on_bioc){
+                        src = '<a target="_blank" href="https://bioconductor.org/packages/release/bioc/html/' + row.name + '.html"><p class="label bioc">bioc</p></a>' + src;
+                    } else {
+                        src = '<p class="label nocran">cran</p>' + src;
+                    }
+                    if (row.onboarding){
+                      src = src + '<a target=\"_blank\" href="' + row.onboarding + '"><i class="fas fa-comments fa-onboarding yes" title = "rOpenSci software review"></i></a>'
+                    } else {
+                      src = src + '<i class="fas fa-comments fa-onboarding no"></i>'
+                    }
+                    return src;
+                },
+                title: "Status"
             },
             {
                 "data": function(row, type, set, meta){return row.keywords || ""},
@@ -112,4 +117,59 @@ $(document).ready( function () {
             return !filter || filter == 'all' || $(oSettings.aoData[iDataIndex].nTr).hasClass(filter);
         }
     );
+
+    var citedata = {};
+    $.get('https://ropenscilabs.github.io/ropensci_citations/citations_all.tsv').done(function(x){
+        x.split("\n").forEach(function(line){
+            var pkgdata = line.split('\t');
+            var pkgname = pkgdata[0];
+            if( ! citedata[pkgname] )
+                citedata[pkgname] = [];
+            citedata[pkgname].push({
+                doi: pkgdata[1],
+                citation: pkgdata[2]
+            });
+        });
+        //console.log(citedata)
+    });
+
+
+    function makeDetailsRow(data){
+        //console.log(data)
+        var text = '<h5>Description</h5><p><i>' + (data.details || "No detail for this package available.") + "</i><p>";
+        if(Object.keys(citedata).length == 0){
+            text = text + "Citation data unavailable";
+            return text;
+        }
+        var citations = citedata[data.name];
+        if(citations){
+            var list = citations.map(function(cite){
+                var doilink = "";
+                if(cite.doi && cite.doi != "NA"){
+                    doilink = '<a href="https://doi.org/' + cite.doi + '">DOI:' + cite.doi + "</a>";
+                }
+                return '<li>' + cite.citation + doilink + '</li>';
+            }).join('\n');
+            text = text + "<br /><h5>Scientific use cases</h5> <ol>" + list + "</ol>";
+        }
+        return '<div class="packagedetails">' + text + '</div>';
+    }
+
+    // Add event listener for opening and closing details
+    $('#packagestable tbody').on('click', 'td.details-control', function () {
+        var tr = $(this).closest('tr');
+        var row = oTable.row( tr );
+        if ( row.child.isShown() ) {
+            $(this).empty().append('<i class="label fa fa-caret-right"></i>')
+            // This row is already open - close it
+            row.child.hide();
+            tr.removeClass('shown');
+        }
+        else {
+            $(this).empty().append('<i class="label fa fa-caret-down"></i>')
+            // Open this row
+            row.child( makeDetailsRow(row.data())).show();
+            tr.addClass('shown');
+        }
+    });
 });
